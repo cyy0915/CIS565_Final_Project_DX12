2cad70e11578f8643636144a9933524376d0c42a
Zhuohao Lin <444053358@qq.com>
2022-12-03 00:18:02 -0500

    finish porting sdf generation code

--- DX12Lib/Resources/Shaders/ComputeShader.hlsl
+++ DX12Lib/Resources/Shaders/ComputeShader.hlsl
@@ -1,5 +1,6 @@
 
 #define BLOCK_SIZE 8
+#define FLT_MAX 3.402823466e+38
 
 struct ComputeShaderInput
 {
@@ -63,9 +64,9 @@ struct BVHNode {
 
 ConstantBuffer<SDF> sdf : register(b0);
 
-StructuredBuffer<BVHNode> nodes : register(s0);
+StructuredBuffer<BVHNode> bvhNodes : register(t0);
 
-StructuredBuffer<Geom> geoms : register(s1);
+StructuredBuffer<Geom> geoms : register(t1);
 
 //RWTexture2D<float4> OutTexture : register(u0);
 RWStructuredBuffer<SDFGrid> sdfGrids : register(u0);
@@ -73,7 +74,53 @@ RWStructuredBuffer<SDFGrid> sdfGrids : register(u0);
 // Linear clamp sampler.
 //SamplerState LinearClampSampler : register(s0);
 
-[numthreads(BLOCK_SIZE, BLOCK_SIZE, 1)]
+
+
+
+float udfTriangle(float3 p, float3 a, float3 b, float3 c)
+{
+    float3 ba = b - a;
+    float3 pa = p - a;
+    float3 cb = c - b;
+    float3 pb = p - b;
+    float3 ac = a - c;
+    float3 pc = p - c;
+    float3 nor = cross(ba, ac);
+
+    float3 ba_pa = ba * clamp(dot(ba, pa) / dot(ba, ba), 0.f, 1.f) - pa;
+    float3 cb_pb = cb * clamp(dot(cb, pb) / dot(cb, cb), 0.f, 1.f) - pb;
+    float3 ac_pc = ac * clamp(dot(ac, pc) / dot(ac, ac), 0.f, 1.f) - pc;
+
+    return sqrt(
+        (sign(dot(cross(ba, nor), pa)) +
+         sign(dot(cross(cb, nor), pb)) +
+         sign(dot(cross(ac, nor), pc)) < 2.f)
+        ?
+        min(min(
+            dot(ba_pa, ba_pa),
+            dot(cb_pb, cb_pb)),
+            dot(ac_pc, ac_pc))
+        :
+        dot(nor, pa) * dot(nor, pa) / dot(nor, nor));
+}
+
+float sdBox(float3 p, float3 b)
+{
+    float3 q = abs(p) - b;
+    return length(max(q, 0.0f)) + min(max(q.x, max(q.y, q.z)), 0.0f);
+}
+
+float udfBox(float3 p, float3 minCorner, float3 maxCorner)
+{
+    float3 center = (maxCorner + minCorner) / 2.f;
+    float3 halfScale = maxCorner - center;
+    return sdBox(p - center, halfScale);
+}
+
+
+
+
+[numthreads(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)]
 void main(ComputeShaderInput IN)
 {
     //DispatchThreadID.xy是二维索引，对应cuda中的索引，可以用这索引直接访问texture对应像素点float4
@@ -82,12 +129,13 @@ void main(ComputeShaderInput IN)
     int y = IN.DispatchThreadID.y;
     int z = IN.DispatchThreadID.z;
 
+    if (x >= sdf.resolution.x || y >= sdf.resolution.y || z >= sdf.resolution.z) return;
     int idx = z * sdf.resolution.x * sdf.resolution.y + y * sdf.resolution.x + x;
 
 
     float3 voxelPos = float3(sdf.minCorner.x + sdf.gridExtent.x * (float(x) + 0.5f),
-                                   sdf.minCorner.y + sdf.gridExtent.y * (float(y) + 0.5f),
-                                   sdf.minCorner.z + sdf.gridExtent.z * (float(z) + 0.5f));
+                             sdf.minCorner.y + sdf.gridExtent.y * (float(y) + 0.5f),
+                             sdf.minCorner.z + sdf.gridExtent.z * (float(z) + 0.5f));
     
     // find closest triangle
     int currIdx = 0;
@@ -97,7 +145,8 @@ void main(ComputeShaderInput IN)
 
     idxToVisit[toVisitOffset++] = 0;
 
-    const Triangle* minTriangle = nullptr;
+    BVHNode minTriangle;
+    minTriangle.geomId = -1;
     float minUdf = FLT_MAX;
 
     while (toVisitOffset > 0)
@@ -107,12 +156,8 @@ void main(ComputeShaderInput IN)
 
         if (!bvhNodes[currIdx].isLeaf)
         {
-            /*float rayLength = boxIntersectionTest(bvhNodes[currIdx].minCorner, bvhNodes[currIdx].maxCorner, r);
-            if (rayLength == -1.f || rayLength > minUdf) continue;*/
-
             float distToBox = udfBox(voxelPos, bvhNodes[currIdx].minCorner, bvhNodes[currIdx].maxCorner);
-            if (distToBox > minUdf)
-                continue;
+            if (distToBox > minUdf) continue;
 
             int leftIdx = currIdx * 2 + 1;
             int rightIdx = leftIdx + 1;
@@ -122,14 +167,13 @@ void main(ComputeShaderInput IN)
         }
         else
         {
-            if (!bvhNodes[currIdx].hasFace)
-                continue;
-            const Triangle* face =  & bvhNodes[currIdx].face;
-            float t = udfTriangle(voxelPos, face.point1, face.point2, face.point3);
+            if (!bvhNodes[currIdx].hasFace) continue;
+            BVHNode face = bvhNodes[currIdx];
+            float t = udfTriangle(voxelPos, face.point1, face.point2, face.point3.xyz);
 
             if (minUdf > t)
             {
-                minTriangle =  & bvhNodes[currIdx].face;
+                minTriangle = bvhNodes[currIdx];
                 minUdf = t;
             }
         }
@@ -137,17 +181,16 @@ void main(ComputeShaderInput IN)
 
 
     // will crash without this
-    if (minTriangle == nullptr)
-        return;
+    if (minTriangle.geomId == -1) return;
     int geomId = minTriangle.geomId;
 
-    float3 triCenter = (minTriangle.point1 + minTriangle.point2 + minTriangle.point3) / 3.f;
-    float3 worldNor = glm::normalize(triCenter - voxelPos);
-    float3 localNor = multiplyMV(geoms[geomId].inverseTransform, float4(worldNor, 0.f));
-
-    float3 localtriNor = multiplyMV(geoms[geomId].inverseTransform, float4(minTriangle.normal1, 0.f));
+    float3 triCenter = (minTriangle.point1 + minTriangle.point2 + minTriangle.point3.xyz) / 3.f;
+    float3 worldNor = normalize(triCenter - voxelPos);
+    float3 localNor = mul(geoms[geomId].inverseTransform, float4(worldNor, 0.f));
+    float3 localtriNor = mul(geoms[geomId].inverseTransform, minTriangle.normal1);
+    
     // if inside
-    if (glm::dot(localNor, localtriNor) > 0.f)
+    if (dot(localNor, localtriNor) > 0.f)
     {
         sdfGrids[idx].dist = -minUdf;
     }
